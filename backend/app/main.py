8a299d37e9ae3d6cd03db4fb9ebbc596be7924c0
Yeshika Dhingra <yeshika11062006@gmail.com>
2026-08-07 15:13:35 +0530

    fin

--- backend/app/main.py
+++ backend/app/main.py
@@ -35,9 +35,9 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "http://18.225.5.73:5173",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "http://18.225.5.73",
     ],
     allow_credentials=True,
     allow_methods=["*"],
